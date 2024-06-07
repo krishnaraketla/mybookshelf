75a3adda3d6ae438c2688b16d33af93d955e3769
raketlak <krishrak97@gmail.com>
2024-06-07 14:18:26 -0400

    Scroll booksheleves

--- frontend/src/components/BookShelf.js
+++ frontend/src/components/BookShelf.js
@@ -8,6 +8,7 @@ const BookShelf = ({ shelfName }) => {
     const [row1, setRow1] = useState([]);
     const [row2, setRow2] = useState([]);
     const [row3, setRow3] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const fetchShelves = async () => {
         try {
@@ -98,30 +99,55 @@ const BookShelf = ({ shelfName }) => {
         }
     };
 
+    const setRows = (page) => {
+        let numPages = 1;
+        if(page<1){
+            setCurrentPage(1)
+            return;
+        }
+        if(books.length <= 9){
+            numPages = 1;
+        }
+        else{
+            numPages = Math.floor(books.length/9) + 1;
+        }
+        if(page > numPages){
+            setCurrentPage(numPages)
+            return;
+        }
+        const booksPerPage = 9;
+        const startIndex = (page - 1) * booksPerPage;
+        const endIndex = startIndex + booksPerPage;
+    
+        const pageBooks = books.slice(startIndex, endIndex);
+    
+        const row1Books = [];
+        const row2Books = [];
+        const row3Books = [];
+    
+        for (let i = 0; i < pageBooks.length; i++) {
+            if (i < 3) {
+                row1Books.push(pageBooks[i]);
+            } else if (i < 6) {
+                row2Books.push(pageBooks[i]);
+            } else {
+                row3Books.push(pageBooks[i]);
+            }
+        }
+    
+        setRow1(row1Books);
+        setRow2(row2Books);
+        setRow3(row3Books);
+    };
+
+
     useEffect(() => {
         fetchShelves();
     }, [shelfName]);
 
     useEffect(() => {
-        const row1Books = [];
-        const row2Books = [];
-        const row3Books = [];
-
-        for (let i = 0; i < books.length; i++) {
-            if (i >= 9) break;
-            if (i < 3) {
-                row1Books.push(books[i]);
-            } else if (i < 6) {
-                row2Books.push(books[i]);
-            } else {
-                row3Books.push(books[i]);
-            }
-        }
-
-        setRow1(row1Books);
-        setRow2(row2Books);
-        setRow3(row3Books);
-    }, [books]);
+        setRows(currentPage)
+    }, [books, currentPage]);
 
     return (
         <div className='book-shelf'>
@@ -130,10 +156,10 @@ const BookShelf = ({ shelfName }) => {
             <BookRow books={row2} />
             <BookRow books={row3} />
             <div className="navigation-arrows">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="rgba(237, 238, 213, 0.5)" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="rgba(237, 238, 213, 0.5)" className="bi bi-chevron-left" viewBox="0 0 16 16" onClick={() => {setCurrentPage(currentPage - 1)}}>
                     <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="rgba(237, 238, 213, 0.5)" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="rgba(237, 238, 213, 0.5)" className="bi bi-chevron-right" viewBox="0 0 16 16" onClick={() => {setCurrentPage(currentPage + 1)}}>
                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </div>
