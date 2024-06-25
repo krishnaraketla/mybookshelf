e3a360d530e260a3be9642ae8fd0d7aca83584d5
raketlak <krishrak97@gmail.com>
2024-06-25 17:22:53 -0400

    goodreads 20k pt1

--- ML/goodreads-20k.py
+++ ML/goodreads-20k.py
@@ -5,18 +5,33 @@ import seaborn as sns
 from tqdm import tqdm
 
 # %%
-# Function to get the total number of lines in a file
 def get_total_lines(file_path):
     with open(file_path, 'r') as f:
         return sum(1 for _ in f)
     
 #%%
-num_lines = get_total_lines("../ML/data/user_id_map.csv")
-num_lines
+def read_json_in_chunks(file_path, chunk_size=500, total_lines=100):
+    total_lines = get_total_lines(file_path)
+    chunk_list = []
+    lines_read = 0
+    with pd.read_json(file_path, lines=True, chunksize=chunk_size) as reader:
+        for chunk in tqdm(reader, desc=f"Loading {file_path}", total=total_lines//chunk_size, unit="chunk"):
+            chunk_list.append(chunk)
+            lines_read += len(chunk)
+            if lines_read >= total_lines:
+                break
+    return pd.concat(chunk_list, ignore_index=True)
+    
 # %%
-num_lines = get_total_lines("../ML/data/book_id_map.csv")
-num_lines
-# %%
-num_lines = get_total_lines("../ML/data/goodreads_interactions.csv")
-num_lines
+goodreads_book_works = read_json_in_chunks('./data/goodreads_book_works.json')
+columns_to_keep = ['work_id', 'best_book_id', 'original_title', 'original_language_id', 'default_description_language_code']
+goodreads_book_works = goodreads_book_works[columns_to_keep]
+goodreads_book_works.to_csv("./data/book_works-full.csv")
+
+#%%
+goodreads_books = read_json_in_chunks('./data/goodreads_books.json')
+# columns_to_keep = ['work_id', 'best_book_id', 'original_title', 'original_language_id', 'default_description_language_code']
+# goodreads_book_works = goodreads_book_works[columns_to_keep]
+goodreads_books.to_csv("./data/books.csv")
+
 # %%
