7987224545ce31b308d06bab7289bea363002e91
raketlak <krishrak97@gmail.com>
2024-07-28 16:19:27 -0400

    CORS-1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = ({setIsAuthenticated}) => {
 
     const handleLogin = async (event) => {
         event.preventDefault();
-        const response = await fetch('https://dry-chamber-11355.herokuapp.com/auth/login', {
+        const response = await fetch(`http://localhost:${process.env.PORT}/auth/login` , {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
